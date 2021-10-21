d5f128ad6d70e67a0c7c834acd3f0e92cde69340
Chris <buesch@imbi.uni-heidelberg.de>
2021-10-21 13:02:05 +0200

    Initial commit

---
+++ R/helloMZK.R
@@ -0,0 +1,18 @@
+# Servus zusammen,
+#
+# Ziel von diesem Package sind Funktionen zu sammeln, welche von allen von uns allen bei der Datenaufbereitung / Analyse
+# in der MZK, aber auch natuerlich ausserhalb, benutzt werden koennen.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, MZK world!")
+}
